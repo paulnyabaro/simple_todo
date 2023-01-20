f5db20bb3b435bd77489c73a680067737edd92a1
paulnyabaro <paulnyabaro1@gmail.com>
2023-01-20 08:20:08 +0300

    Created index template

--- simple_todo/base/views.py
+++ simple_todo/base/views.py
@@ -5,7 +5,7 @@ from .models import ToDoList, Item
 def index(request):
     todo_items = ToDoList.objects.all()
     context = {'todo_items': todo_items}
-    return render(request, 'base/base.html', context)
+    return render(request, 'base/index.html', context)
 
 def todo(request):
     context = {'todos': todos}
