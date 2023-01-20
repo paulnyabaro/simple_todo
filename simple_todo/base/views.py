1315fc8d2a03930faf42f38ce826aaa3fa1c03db
paulnyabaro <paulnyabaro1@gmail.com>
2023-01-20 08:36:08 +0300

    Added active page link

--- simple_todo/base/views.py
+++ simple_todo/base/views.py
@@ -4,11 +4,12 @@ from .models import ToDoList, Item
 
 def index(request):
     todo_items = ToDoList.objects.all()
-    context = {'todo_items': todo_items}
+    context = {'page': 'index', 'todo_items': todo_items}
     return render(request, 'base/index.html', context)
 
 def todo(request):
     pass
 
 def create(request):
-    return render(request, 'base/create.html')
+    context = {'page': 'create'}
+    return render(request, 'base/create.html', context)
