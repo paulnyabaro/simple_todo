069908a2323530a8c3f372ee0e5e4bbf631a2e4d
paulnyabaro <paulnyabaro1@gmail.com>
2023-01-23 07:03:59 +0300

    Getting and saving a new todolist from the form

--- simple_todo/base/views.py
+++ simple_todo/base/views.py
@@ -14,6 +14,10 @@ def todo(request):
 def create(request):
     if request.method == 'POST':
         form = CreateNewList(request.POST) # Create a for that has those characters populated
+        if form.is_valid():
+            n = form.cleaned_data['name']
+            t = ToDoList(name=n)
+            t.save()
     else:
         pass
     form = CreateNewList()
