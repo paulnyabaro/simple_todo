4f130d0669bd7bd8d9e92ff599ce27e12c04b88a
paulnyabaro <paulnyabaro1@gmail.com>
2023-01-25 06:30:25 +0300

    Registered crispy forms for styling forms

--- simple_todo/simple_todo/settings.py
+++ simple_todo/simple_todo/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms', # Registering crispy forms for styling forms
     'base.apps.BaseConfig', # Registering base app
     'register.apps.RegisterConfig', # Registering register app
 ]
