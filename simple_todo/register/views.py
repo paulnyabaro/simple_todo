34227c50ff6e23d590966dbc18ef3cb8997bc453
paulnyabaro <paulnyabaro1@gmail.com>
2023-01-25 05:56:27 +0300

    Added registration form

--- simple_todo/register/views.py
+++ simple_todo/register/views.py
@@ -3,6 +3,12 @@ from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
 
 def register(request):
-    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+    else:
+        form = UserCreationForm()
+
     context = {'form': form}
     return render(request, 'register/register.html', context)
