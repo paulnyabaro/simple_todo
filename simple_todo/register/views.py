41bce46fe3b7383d935704919fabf58df1cbd173
paulnyabaro <paulnyabaro1@gmail.com>
2023-01-25 06:02:26 +0300

    Added registration form

--- simple_todo/register/views.py
+++ simple_todo/register/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
 
@@ -7,6 +7,7 @@ def register(request):
         form = UserCreationForm(request.POST)
         if form.is_valid():
             form.save()
+        return redirect('index')
     else:
         form = UserCreationForm()
 
