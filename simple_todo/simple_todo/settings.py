d0727f47a4b85176cdc2d15cd0400d64066e879a
paulnyabaro <paulnyabaro1@gmail.com>
2023-01-25 06:33:53 +0300

    Defined crispy template pack

--- simple_todo/simple_todo/settings.py
+++ simple_todo/simple_todo/settings.py
@@ -124,3 +124,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap5' # Defining crispy template pack to be used
